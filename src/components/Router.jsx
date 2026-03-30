5f4320d564909c3a0634db21af87ed27cfe1e763
Fany31-dev <s.rambeault@gmx.fr>
2026-03-30 19:12:24 +0200

    ajout components Router.jsx

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home.jsx';
-import About from '../pages/About.jsx';
-import Logements from '../pages/Logements.jsx';
-import Error from '../pages/Error.jsx';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Logements from '../pages/Logements';
+import Error from '../pages/Error';
 
 function Router() {
   return (
@@ -11,6 +11,7 @@ function Router() {
       <Route path="/about" element={<About />} />
       <Route path="/logements" element={<Logements />} />
       <Route path="*" element={<Error />} />
+      {/* si pas d'url trouvé erreur 404 */}
     </Routes>
   );
 }
