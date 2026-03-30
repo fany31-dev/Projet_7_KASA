665cb28069810aca3b5b024db851918e58f6ab3d
Fany31-dev <s.rambeault@gmx.fr>
2026-03-30 10:36:04 +0200

    creation du routage des pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Error from './pages/Error.jsx';
+import Logements from './pages/Logements.jsx';
 
 function App() {
-  return <h1>Bonjour</h1>;
+  return (
+    <div>
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/about">À propos</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logements" element={<Logements />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
