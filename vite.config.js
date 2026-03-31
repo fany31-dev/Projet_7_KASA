a994753f91a785c4070b7402da33e238d6f5ad72
Fany31-dev <s.rambeault@gmx.fr>
2026-03-31 18:01:59 +0200

    configuration fichiers vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+ css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "/src/styles/variables";`
+      }
+    }
+  },
+
+
+resolve: {
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  },
+},
+
 })
+
