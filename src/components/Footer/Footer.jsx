beeb4abbcb1d2fbe1f28f87afdc501321b2fd086
Fany31-dev <s.rambeault@gmx.fr>
2026-04-01 12:03:09 +0200

    ajout du logo Kasa dans components Header et Footer + premier element de style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import LogoKasaBlanc from '@/assets/images/Logo_blanc.png';
+import styles from './Footer.module.scss';
 
 function Footer() {
   return (
-    <footer>
-      <div>Logo KASA</div>
-      <div>icone c</div>
-      <div>2020 Kasa. All rights reserved</div>
-    </footer>
+    <>
+      <footer className={styles.footer}>
+        <div>
+          <img
+            className={styles.footer__logo}
+            src={LogoKasaBlanc}
+            alt="Logo Kasa"
+          />
+        </div>
+        <p>© 2020 Kasa. All rights reserved</p>
+      </footer>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import LogoKasaOrange from '@/assets/images/Logo_orange.png';
 import styles from './Header.module.scss';
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <div>Logo KASA</div>
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">À propos</Link>
-      </nav>
-    </header>
+    <>
+      <header className={styles.header}>
+        <div>
+          <img
+            src={LogoKasaOrange}
+            alt="Logo Kasa"
+            className={styles.header__logo}
+          />
+        </div>
+        <nav className={styles.header__navigation}>
+          <Link to="/">Accueil</Link>
+          <Link to="/about">À propos</Link>
+        </nav>
+      </header>
+    </>
   );
 }
 
