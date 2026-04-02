97e51ffcbe401f36b9479cd0cdfee1d64cd1862f
Fany31-dev <s.rambeault@gmx.fr>
2026-04-02 16:27:26 +0200

    modification link => navLink / fonctionnement navigation + style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,43 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import LogoKasaOrange from '@/assets/images/Logo_orange.png';
 import styles from './Header.module.scss';
 
 function Header() {
   return (
     <>
-      <header className={styles.header}>
-        <div>
-          <img
-            src={LogoKasaOrange}
-            alt="Logo Kasa"
-            className={styles.header__logo}
-          />
-        </div>
-        <nav className={styles.header__navigation}>
-          <Link to="/">Accueil</Link>
-          <Link to="/about">À propos</Link>
-        </nav>
-      </header>
+      <div className={styles.header}>
+        <header className={styles.header__container}>
+          <div>
+            <img
+              src={LogoKasaOrange}
+              alt="Logo Kasa"
+              className={styles.header__logo}
+            />
+          </div>
+          <nav className={styles.header__navigation}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles.nav__link} ${styles.active}`
+                  : styles.nav__link
+              }
+            >
+              Accueil
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles.nav__link} ${styles.active}`
+                  : styles.nav__link
+              }
+            >
+              A propos
+            </NavLink>
+          </nav>
+        </header>
+      </div>
     </>
   );
 }
