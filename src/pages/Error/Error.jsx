1df0e3dec498d98825f79258e9809e7425fdcc8b
Fany31-dev <s.rambeault@gmx.fr>
2026-04-02 16:26:25 +0200

    ajout style pages Error + modif className

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -3,10 +3,12 @@ import './Error.scss';
 
 function Error() {
   return (
-    <div className="error-container">
-      <h1 className="error-title">404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/" className="error-link">
+    <div className="error">
+      <h1 className="error__title">404</h1>
+      <p className="error__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error__link">
         Retourner sur la page d'accueil
       </Link>
     </div>
