a23a2840421526eef6f5a79132db22c881fd9045
Fany31-dev <s.rambeault@gmx.fr>
2026-04-02 11:55:54 +0200

    creation banner.jsx + styles banner.module.scss

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import BannerImage from '@/assets/images/paysage_falaise.png';
+import styles from './Banner.module.scss';
+
+function Banner() {
+  return (
+    <>
+      <div className={styles.banner}>
+        <img
+          className={styles.banner__image}
+          src={BannerImage}
+          alt="Banniere paysage de falaise au bord de l'océan"
+        />
+        <p className={styles.banner__text}>Chez nous, partout et ailleurs</p>
+      </div>
+    </>
+  );
+}
+
+export default Banner;
