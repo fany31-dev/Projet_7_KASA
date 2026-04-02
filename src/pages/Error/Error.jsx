79e7382c1e9c91700a3e004d52140c8925c54837
Fany31-dev <s.rambeault@gmx.fr>
2026-04-02 11:59:42 +0200

    ajout div error container => message d'erreur maquette Figma dans pages/Error

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
 import './Error.scss';
 
 function Error() {
-  return <h1> erreur 404</h1>;
+  return (
+    <div className="error-container">
+      <h1 className="error-title">404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error-link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 }
+
 export default Error;
