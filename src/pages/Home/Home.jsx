64d9f83e08e2eef67916126ea1da05bf678ed624
Fany31-dev <s.rambeault@gmx.fr>
2026-04-09 15:05:42 +0200

    ajout card dans page Home.jsx + style desktop et mobile scss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 import Banner from '../../components/Banner/Banner';
+import logements from '@/data/logements.json';
+import Card from '../../components/Card/Card';
 import './Home.scss';
 
 function Home() {
   return (
-    <div className="container">
-      <Banner />
-    </div>
+    <>
+      <div className="container">
+        <Banner />
+        <div className="container__card">
+          {logements.map(({ id, title, cover }) => (
+            <Card
+              key={id}
+              id={id}
+              title={title}
+              imageUrl={cover}
+              altText={`Photo du logement ${title}`}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
