f67e6121e662b3244f661ef7aa30ecbd56fd39d2
Fany31-dev <s.rambeault@gmx.fr>
2026-04-10 14:54:18 +0200

    modification de l'element Banner dans page Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from '../../components/Banner/Banner';
+import BannerImage from '@/assets/images/paysage_falaise.png';
 import logements from '@/data/logements.json';
 import Card from '../../components/Card/Card';
 import './Home.scss';
@@ -7,7 +8,13 @@ function Home() {
   return (
     <>
       <div className="container">
-        <Banner />
+        <div>
+          <Banner
+            image={BannerImage}
+            alt="Banniere paysage de falaise au bord de l'océan"
+            texte="Chez nous, partout et ailleurs"
+          />
+        </div>
         <div className="container__card">
           {logements.map(({ id, title, cover }) => (
             <Card
