07accbb0382da179f26365eb7b1cd203312c582b
Fany31-dev <s.rambeault@gmx.fr>
2026-04-13 16:11:39 +0200

    import collapse + banner dans page about + style lié

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,50 @@
+import BannerAPropos from '@/assets/images/paysage_montagne.png';
+import Banner from '../../components/Banner/Banner';
+import Collapse from '../../components/Collapse/Collapse';
 import './About.scss';
 
-function About() {
-  return <h1>A Propos</h1>;
+function About({ image, title, texte }) {
+  return (
+    <>
+      <div className="container">
+        <Banner image={BannerAPropos} alt="paysage de montagne" />
+
+        <div>
+          <Collapse
+            title="Fiabilité"
+            texte="Les annonces postées sur Kasa garantissent une fiabilité
+            totale. Les photos sont conformes aux logements, et toutes les
+            informations sont régulièrement vérifiées par nos équipes."
+          ></Collapse>
+
+          <Collapse
+            title="Respect"
+            texte="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement discriminatoire ou de perturbation du voisinage
+              entraînera une exclusion de notre plateforme."
+          ></Collapse>
+
+          <Collapse
+            title="Service"
+            texte="La qualité du service est au cœur de notre engagement chez Kasa.
+              Nous veillons à ce que chaque interaction, que ce soit avec nos
+              hôtes ou nos locataires, soit empreinte de respect et de
+              bienveillance."
+          ></Collapse>
+
+          <Collapse
+            title="Sécurité"
+            texte="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+              pour les voyageurs, chaque logement correspond aux critères de
+              sécurité établis par nos services. En laissant une note aussi bien
+              à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+              que les standards sont bien respectés. Nous organisons également
+              des ateliers sur la sécurité domestique pour nos hôtes."
+          ></Collapse>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
