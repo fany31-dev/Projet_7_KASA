7fc5515afa1bad6d6ff4ad1993d9fc07898030b6
Fany31-dev <s.rambeault@gmx.fr>
2026-04-29 18:56:35 +0200

    creation collapse pour ficheLogement avec props isLogementPage

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import ArrowClose from '@/assets/icones/arrow_close_24px.png';
 import styles from './Collapse.module.scss';
 
-function Collapse({ title, texte }) {
+function Collapse({ title, texte, isLogementPage }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
@@ -11,7 +11,9 @@ function Collapse({ title, texte }) {
 
   return (
     <>
-      <div className={styles.collapse}>
+      <div
+        className={`${styles.collapse} ${isLogementPage ? styles.collapse__ficheLogement : ''}`}
+      >
         <div className={styles.collapse__title} onClick={toggle}>
           <h2>{title}</h2>
 
