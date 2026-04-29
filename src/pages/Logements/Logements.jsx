ff3942d686713a941e79da4ac6af3a0edd15312e
Fany31-dev <s.rambeault@gmx.fr>
2026-04-29 18:58:14 +0200

    amelioration ficheLogement ajout scss collapse + visuel carrousel + debut scss de la page

--- src/pages/Logements/Logements.jsx
+++ src/pages/Logements/Logements.jsx
@@ -17,27 +17,37 @@ function Logements() {
       <div className="ficheLogement">
         <Banner
           pictures={logement.pictures}
+          noFilter={true}
+          isCarrousel={true}
           alt={logement.title}
-          texte={logement.title}
         />
-
-        <div className="ficheLogement__title">
-          <h1 className="ficheLogement__title__name">{logement.title}</h1>
-          <p className="ficheLogement__title__localisation">
-            {logement.location}
-          </p>
-        </div>
-        <div className="ficheLogement__host">
-          <p className="ficheLogement__host__name">{logement.host.name}</p>
-          <img
-            className="ficheLogement__title__localisation"
-            src={logement.host.picture}
-            alt={logement.host.name}
-          />
+        <div className="ficheLogement__container">
+          <div className="ficheLogement__title">
+            <h1 className="ficheLogement__title__name">{logement.title}</h1>
+            <p className="ficheLogement__title__localisation">
+              {logement.location}
+            </p>
+          </div>
+          <div className="ficheLogement__host">
+            <p className="ficheLogement__host__name">{logement.host.name}</p>
+            <img
+              className="ficheLogement__host__picture"
+              src={logement.host.picture}
+              alt={logement.host.name}
+            />
+          </div>
         </div>
         <div className="ficheLogement__description">
-          <Collapse title="Description" texte={logement.description}></Collapse>
-          <Collapse title="Equipements" texte={logement.equipments}></Collapse>
+          <Collapse
+            isLogementPage={true}
+            title="Description"
+            texte={logement.description}
+          ></Collapse>
+          <Collapse
+            isLogementPage={true}
+            title="Equipements"
+            texte={logement.equipments}
+          ></Collapse>
         </div>
       </div>
     </>
