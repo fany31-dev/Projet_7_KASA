df551941aaccb233afb443b3be03e5cb9e6bc41f
Fany31-dev <s.rambeault@gmx.fr>
2026-04-29 20:15:14 +0200

    ajout layout dans page d'erreur pour mise en page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
 import './Error.scss';
+import Layout from '../../components/Layout/Layout';
 
 function Error() {
   return (
-    <div className="error">
-      <h1 className="error__title">404</h1>
-      <p className="error__text">
-        Oups! La page que vous demandez n'existe pas.
-      </p>
-      <Link to="/" className="error__link">
-        Retourner sur la page d'accueil
-      </Link>
-    </div>
+    <layout mainClassName="error__main">
+      <div className="error">
+        <h1 className="error__title">404</h1>
+        <p className="error__text">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to="/" className="error__link">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+    </layout>
   );
 }
 
