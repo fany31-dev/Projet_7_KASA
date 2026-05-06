c82467eb4ab96a6081faf6187ce70adde94c2bfb
Fany31-dev <s.rambeault@gmx.fr>
2026-05-06 11:03:05 +0200

    modification style page Error

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -4,17 +4,15 @@ import Layout from '../../components/Layout/Layout';
 
 function ErrorPage() {
   return (
-    <layout mainclassname="error__main">
-      <div className="error">
-        <h1 className="error__title">404</h1>
-        <p className="error__text">
-          Oups! La page que vous demandez n'existe pas.
-        </p>
-        <Link to="/" className="error__link">
-          Retourner sur la page d'accueil
-        </Link>
-      </div>
-    </layout>
+    <div className="error">
+      <h1 className="error__title">404</h1>
+      <p className="error__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error__link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
   );
 }
 
