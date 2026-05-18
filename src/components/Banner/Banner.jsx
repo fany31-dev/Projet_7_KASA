a415576338120867e2f033add545d17596fec250
Fany31-dev <s.rambeault@gmx.fr>
2026-05-18 15:38:52 +0200

    modification element Banner + style ( separation Banner et Carousel en deux components)

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,64 +1,15 @@
-import { useState, useEffect } from 'react';
-import ArrowBack from '@/assets/icones/arrow_back.png';
-import ArrowNext from '@/assets/icones/arrow_next.png';
-import DataLogement from '@/data/logements.json';
+import BannerImage from '@/assets/images/paysage_falaise.png';
 import styles from './Banner.module.scss';
 
-function Banner({ image, alt, texte, pictures, noFilter, isCarrousel }) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const images = pictures?.length ? pictures : image ? [image] : [];
-
-  const prevPicture = () => {
-    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-  };
-
-  const nextPicture = () => {
-    setCurrentIndex((currentIndex + 1) % images.length);
-  };
-
-  useEffect(() => {
-    if (!isCarrousel || images.length <= 1) return;
-
-    const interval = setInterval(() => {
-      setCurrentIndex((prev) => (prev + 1) % images.length);
-    }, 3000); // toutes les 3 secondes
-    return () => clearInterval(interval);
-  }, [images.length, isCarrousel]);
-
+function Banner({ image, texte }) {
   return (
     <>
-      <div
-        className={`${styles.banner} ${isCarrousel ? styles.carrouselHeight : ''}`}
-      >
+      <div className={styles.banner}>
         <img
           className={styles.banner__image}
-          style={{ filter: noFilter ? 'none' : 'brightness(0.6)' }}
-          src={images[currentIndex]}
-          alt={alt || 'Bannière'}
+          src={image || BannerImage}
+          alt="Banniere paysage de falaise au bord de l'océan"
         />
-        {images.length > 1 && (
-          <>
-            <button
-              onClick={prevPicture}
-              className={`${styles.banner__arrow} ${styles.banner__arrow__left}`}
-              aria-label="Image précédente"
-            >
-              <img src={ArrowBack} alt={'Image précédente'} />
-            </button>
-
-            <button
-              onClick={nextPicture}
-              className={`${styles.banner__arrow} ${styles.banner__arrow__right}`}
-              aria-label="Image suivante"
-            >
-              <img src={ArrowNext} alt={'Image suivante'} />
-            </button>
-            <span className={styles.banner__counter}>
-              {currentIndex + 1} / {images.length}
-            </span>
-          </>
-        )}
         {texte && <h1 className={styles.banner__text}>{texte}</h1>}
       </div>
     </>
